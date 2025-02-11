50e4e561e465fd08bf7e2c7246336283d8abc0ea
kito-mi <ayoubradjetti@gmail.com>
2025-02-11 13:18:50 +0100

    Delete check_bot.py

--- check_bot.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TOKEN = '8111627355:AAEOP-AzwPN17MAaUH_2Doel5bZxn0jXIPI'
-
-url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-
-response = requests.get(url)
-print(response.json())
